466a6098a9fc00fdb869eded0b6b2b0a9f59101f
Matthew McPherrin <matt@mcpherrin.ca>
2013-06-09 01:53:43 -0400

    hawkward notes

--- net/test.cc
+++ net/test.cc
@@ -18,10 +18,20 @@ int main(int argc, char **argv) {
     char buf[256];
     Message m;
   };
+  // Hawkward:  That buffer m could become
+  // invalid if the network is slow or whatever
+  // and this function returns.
+
+  // In general, since "send" is async, I need to be careful.
+  // But I don't have a way of telling if a message has been sent,
+  // so managing buffers is grosslike.
+
+  // I could mandate Message be a new'd object for later delete'ion
+  // on successful send, or otherwise come from a specified pool.  Given
+  // convenient ways of using those, it might not be bad.  
   scanf("%s", buf+sizeof(m));
   m.typetag = MessageType::DEBUG;
   c.send(&m, 256);
 
-  char buffa[256];
-  scanf("%s", buffa);
+  std::this_thread::sleep_for(std::chrono::hours(999));
 }
