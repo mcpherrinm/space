8acfa638f5592382968fa631e6ce520990833de8
Matthew McPherrin <matthew@mcpherrin.ca>
2013-07-02 03:57:22 -0400

    start making API: policy based

--- net/net.hh
+++ net/net.hh
@@ -33,8 +33,6 @@ public:
 };
 
 // This is what lives in the various queues.
-// The header is followed by the message
-// so don't copy it off!
 struct QueuedMessage {
   std::chrono::time_point<std::chrono::steady_clock> time_in_Q;
   int resends;
@@ -43,7 +41,8 @@ struct QueuedMessage {
   ssize_t data_len; // includes size of header
 };
 
-class Connection {
+template <class CompletionPolicy, class DispatchPolicy>
+class Connection: public CompletionPolicy, public DispatchPolicy {
   Address server;
   int sock;
   int nextmessage = 0;
@@ -54,17 +53,133 @@ class Connection {
 
   std::mutex ack_Q_mutex;
   std::list<QueuedMessage> ack_Q;
-  bool ack_msg(int);
-
-  void send_loop();
-  void recv_loop();
-
   std::thread send_thread;
   std::thread recv_thread;
 
 public:
-  Connection(const Address &server);
+  Connection(const Address &server_): server(server_) {
+    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-  bool send(Message*, ssize_t len);
-  void finish(); // terminate and join threads
+    send_thread = std::thread(&Connection<CompletionPolicy,DispatchPolicy>::send_loop, this);
+    recv_thread = std::thread(&Connection<CompletionPolicy,DispatchPolicy>::recv_loop, this);
+    send_thread.detach();
+    recv_thread.detach();
+  }
+
+private:
+  bool ack_msg(int id) {
+    std::lock_guard<std::mutex> lk(ack_Q_mutex);
+    for(auto i = ack_Q.begin(); i != ack_Q.end(); i++) {
+      if(i->data->messageid == id) {
+        //disposeMessage(i->data);
+        ack_Q.erase(i);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // On the new thread, do a select() loop
+  void recv_loop() {
+    while(1) {
+      union {
+        char recvbuf[1500];
+        Message incoming;
+      };
+      Address from;
+      socklen_t fromlen = sizeof from.sa;
+      ssize_t recvsize = recvfrom(sock,
+                                  recvbuf, sizeof recvbuf,
+                                  0,
+                                  (struct sockaddr*)&from.sa, &fromlen);
+
+      // TODO sort out dispatching
+      if(incoming.typetag == MessageType::ACK) {
+        ack_msg(incoming.messageid);
+      } else {
+        DispatchPolicy::dispatch(&incoming, recvsize, &from);
+      }
+    }
+  }
+
+  // This is pretty gross/bad.
+  void send_loop() {
+    while(1) {
+      bool send = false;
+      QueuedMessage m, ackm;
+      {
+        std::unique_lock<std::mutex> lk(send_Q_mutex);
+        if(send_Q.empty()) {
+          if(ack_Q.empty()) {
+            send_Q_cv.wait(lk);
+          } else {
+            // Maybe, more appropriately, wait until the next ack timeout.
+            send_Q_cv.wait_for(lk, std::chrono::milliseconds(31));
+          }
+        }
+        if(!send_Q.empty()) {
+          m = send_Q.front();
+          send_Q.pop();
+          send = true;
+        }
+      }
+      bool resend = false;
+      {
+        std::lock_guard<std::mutex> lk(ack_Q_mutex);
+        if(send) {
+          m.time_in_Q = std::chrono::steady_clock::now();
+          ack_Q.push_back(m);
+        }
+
+        // check if any unacked packets need to be resent.
+        if(!ack_Q.empty()) {
+          ackm = ack_Q.front();
+          auto now = std::chrono::steady_clock::now();
+          auto count = std::chrono::duration_cast<std::chrono::milliseconds>(now - ackm.time_in_Q).count();
+          if(count > 30) {
+            ack_Q.erase(ack_Q.begin());
+            resend = true;
+          }
+        }
+      }
+      if(resend) {
+        ackm.resends += 1;
+        if(ackm.resends > 10) {
+          printf("Stopping resending at 10 -- need to handle this\n");
+          return;
+        }
+        std::lock_guard<std::mutex> lk(send_Q_mutex);
+        send_Q.push(ackm);
+      }
+      if(send) {
+        if(m.data->typetag == MessageType::INVALID) {
+          printf("Tried to send invalid message\n");
+          return;
+        }
+        ssize_t sent = sendto(sock,
+                              m.data,
+                              m.data_len,
+                              0, /*flags*/
+                              (struct sockaddr*)&server.sa,
+                              sizeof server.sa);
+        if(sent < 0) {
+          perror("sendto");
+          return;
+        }
+      }
+    }
+  }
+
+public:
+  bool send(Message *msg, ssize_t datalen) {
+    std::lock_guard<std::mutex> lk(send_Q_mutex);
+    QueuedMessage m;
+    m.data = msg;
+    m.data_len = datalen;
+    msg->messageid = ++nextmessage;
+    send_Q.push(m);
+    send_Q_cv.notify_one();
+    return true;  // If I switch to a bounded Queue, return false on full
+                  // or on dead connection
+  }
 };
--- net/test.cc
+++ net/test.cc
@@ -2,28 +2,29 @@
 
 #include "net.hh"
 
+class DeleteCompletion {
+  void dispose(Message *m){delete m;};
+};
+
+class Dispatcher {
+  public:
+  void dispatch(Message *m, ssize_t len, Address *from) {
+    printf("Got a message of size %ld from %s of type %d\n",
+      len, from->ip_str(), (int)m->typetag);
+  }
+};
+
 int main(int argc, char **argv) {
   Address testserv(argc > 1 ? argv[1] : "127.0.0.1");
-  Connection c(testserv);
+  Connection<DeleteCompletion,Dispatcher> c(testserv);
 
   JoinMessage *join = new JoinMessage("John");
-  c.send((Message*)join, sizeof join);
+  c.send((Message*)join, sizeof(JoinMessage));
 
   union {
     char buf[256];
     Message m;
   };
-  // Hawkward:  That buffer m could become
-  // invalid if the network is slow or whatever
-  // and this function returns.
-
-  // In general, since "send" is async, I need to be careful.
-  // But I don't have a way of telling if a message has been sent,
-  // so managing buffers is grosslike.
-
-  // I could mandate Message be a new'd object for later delete'ion
-  // on successful send, or otherwise come from a specified pool.  Given
-  // convenient ways of using those, it might not be bad.  
   scanf("%s", buf+sizeof(m));
   m.typetag = MessageType::DEBUG;
   c.send(&m, 256);
